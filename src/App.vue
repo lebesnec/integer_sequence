26374b6a11fe9b6a49dfb6ad55910b72cce81723
Christophe Le Besnerais <lebesnec@users.noreply.github.com>
2020-04-01 12:04:14 +0200

    aa

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,15 @@
 
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
-        <span class="hidden-sm-and-down">Integer sequence</span>
+      <v-toolbar-title class="ml-0 pl-4 pr-4 hidden-sm-and-down">
+        <span >Integer sequence</span>
       </v-toolbar-title>
-      <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search" class="hidden-sm-and-down"/>
-      <v-spacer/>
+      <!-- <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search"/> -->
+      <v-autocomplete v-model="select" :loading="loading" :items="items" :search-input.sync="search" cache-items
+            hide-no-data flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" label="Search">
+      </v-autocomplete>
+
+      <v-spacer class="hidden-sm-and-down"/>
     </v-app-bar>
 
     <v-content>
@@ -30,7 +34,28 @@
     name: "App",
     components: { },
     data: () => ({
-      drawer: null
-    })
+      drawer: null,
+      loading: false,
+      items: [],
+      search: null,
+      select: null
+    }),
+    watch: {
+      search(val) {
+        this.isLoading = true;
+
+        fetch(`https://oeis.org/search?q=${val}&fmt=json`, { method: "HEAD", mode: "no-cors" })
+        .then((res) => {
+          console.log(res);
+          this.items = res.json().data;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.isLoading = false
+        });
+      }
+    }
   };
 </script>
